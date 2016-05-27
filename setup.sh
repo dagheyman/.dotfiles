f94c57ff99003b043b03139831295c3414fdc7ed
Dag Heyman <dag@yubico.com>
2016-05-27 12:03:44 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run this from your home directory to link to the configuration files.
+
+set -e
+
+# Check that the directory is right.
+if [ "$PWD" != ~ ]
+then
+    echo "Not in home directory."
+    exit 1
+fi
+
+# Create the symlinks. Will replace current configuration files.
+echo "Creating links..."
+ln -sf .dotfiles/.bashrc
+ln -sf .dotfiles/.vimrc
+echo "...done."
+
+exit 0
+
