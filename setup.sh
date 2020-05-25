044f0b060f771e4d0a813f6ca96f68b270751b1a
Dag Heyman <dag@yubico.com>
2020-05-25 07:47:09 +0200

    zsh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ fi
 # Create the symlinks. Will replace current configuration files.
 echo "Creating links..."
 ln -sf .dotfiles/.bashrc
+ln -sf .dotfiles/.zshrc
 ln -sf .dotfiles/.vimrc
 ln -sf .dotfiles/.tmux.conf
 ln -sf .dotfiles/.gdbinit
